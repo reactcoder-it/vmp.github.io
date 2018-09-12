e247aaeafc113db0ebc1812c6699a6f329edd4bc
Vadim Pashaev <vmp@live.ru>
2018-09-12 05:32:11 +0400

    Добавил обработчики ошибок и отсутствующих страниц

--- routes/blog.js
+++ routes/blog.js
@@ -6,22 +6,35 @@ const router = express.Router();
 const texts_path = './texts';
 
 router.get('/', function (req, res) {
-    fs.readdir(texts_path, function(err, files) {
-        res.render('jokes', {
-            files: files,
-            title: "Блог",
-            jokes: false
-        });
+  fs.readdir(texts_path, function(err, files) {
+    res.render('jokes', {
+      files: files,
+      title: "Блог",
+      jokes: false
     });
+  });
 });
 
-router.get('/:file', function (req, res) {
-    fs.readFile(texts_path + '/' + req.params.file, function (err, data) {
-        if (err) throw err;
-        res.render('joke', {
-            data: data
-        });
+router.get('/:file', function (req, res, next) {
+  fs.readFile(texts_path + '/' + req.params.file, function (err, data) {
+    if (err) return next();
+    res.render('joke', {
+      data: data
     });
+  });
+});
+
+// Отслеживаем 404 ошибку и перенаправляем в обработчик ошибок
+router.use(function(req, res, next) {
+  res.status(404);
+  res.render('404');
+});
+
+// Обработчик ошибок
+router.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error');
+  console.log(err);
 });
 
 module.exports = router;
